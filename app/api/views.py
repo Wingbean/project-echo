c76f894cbcc6bfb90938120c7eb470145b48a7bf
Wingbean <taeey.com@gmail.com>
2026-02-26 15:37:19 +0700

    refactor: Consolidate query and results functionality into the main index page, removing dedicated templates and routes.

--- app/api/views.py
+++ app/api/views.py
@@ -1,7 +1,7 @@
 # app/api/views.py - Page Rendering Routes
-from flask import render_template, request
+from flask import render_template
 from app.api import views_bp
-from app.services.render_service import get_dashboard_data, get_table_data
+from app.services.render_service import get_dashboard_data
 
 
 @views_bp.route("/")
@@ -9,22 +9,6 @@ def index():
     """Homepage / Dashboard."""
     data = get_dashboard_data()
     return render_template("pages/index.html", data=data)
-
-
-@views_bp.route("/query")
-def query_page():
-    """Query input page."""
-    return render_template("pages/query.html")
-
-
-@views_bp.route("/results")
-def results_page():
-    """Results display page."""
-    table_name = request.args.get("table", "")
-    data = {}
-    if table_name:
-        data = get_table_data(table_name)
-    return render_template("pages/results.html", data=data, table_name=table_name)
 
 
 @views_bp.route("/consult")
